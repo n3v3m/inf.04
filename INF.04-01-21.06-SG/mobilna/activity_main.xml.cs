143c5e321bd8bdb1bd0673e23d77b5e7d402cff9
n3v3m <92385235+n3v3m@users.noreply.github.com>
2025-01-26 16:38:28 +0100

    maui addon

---
+++ INF.04-01-21.06-SG/mobilna/activity_main.xml.cs
@@ -0,0 +1,35 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void funkcjaZatwierdz(object sender, EventArgs e)
+        {
+            if (email.Text.Contains("@"))
+            {
+                if (haslo.Text == repeatHaslo.Text)
+                {
+                    komunikat.Text = "Witaj " + email.Text;
+                }
+                else
+                {
+                    komunikat.Text = "Hasła się różnią";
+                }
+            }
+            else
+            {
+                komunikat.Text = "Nieprawidłowy adres e - mail";
+            }
+
+        }
+
+
+
+    }
+
+}
